ab4e65f049f6df3ffabbb9ccdf5450845bf4591d
Joe Smith <joeycrushed@gmail.com>
2021-07-29 15:37:45 +0100

    static react expenses app

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card'
 import './ExpenseItem.css'
 
 
@@ -9,13 +10,13 @@ function ExpenseItem(props) {
     const year = props.date.getFullYear();
 
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">£{props.amount}</div>
             </div>
-        </div>
+        </Card>
 
     )
 }
